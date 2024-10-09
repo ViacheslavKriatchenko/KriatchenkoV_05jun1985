450b0ab542fe95a32461940c86a685d01e630fa5
Slavencia <kryatchenko.vv@gmail.com>
2024-10-09 14:37:35 +0300

    updated

--- api/altavita_cart.py
+++ api/altavita_cart.py
@@ -1,5 +1,16 @@
 import requests
 
-response = requests.post(url="",
-                        json="",
-                        )
+
+class Cart():
+
+    def get_cart_info():
+        response = requests.post(url="",
+                                json="",
+                                )
+
+        if response.status_code == 200:
+                return response.json()
+        else:
+            raise Exception(
+                f"API request failed with status code {response.status_code}"
+                )
